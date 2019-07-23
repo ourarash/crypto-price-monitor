e5b7467c7a7ffb7042721a49db4928ff32354042
Ari <ourarash@gmail.com>
2019-07-22 22:12:59 -0700

    update notification

--- examples/termux.js
+++ examples/termux.js
@@ -44,7 +44,7 @@ async function updateValuesCallback(notificationOutput, mktCapFormatted) {
 async function updateNotification() {
   g_updateCounter++;
   
-  let frames = spinners.simpleDots.frames;
+  let frames = spinners.star.frames;
  
     
   if (
